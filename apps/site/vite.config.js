ae90a2483027f11a35bc4d9a9d13938e32967a51
Ameer Jhan <ameerjhanprof@gmail.com>
2024-11-27 12:35:08 +0530

    chore: improve chunking

--- apps/site/vite.config.js
+++ apps/site/vite.config.js
@@ -11,4 +11,17 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
     },
   },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: (id) => {
+          if (id.includes('monaco-editor')) {
+            return 'monaco-editor';
+          } else if (id.includes('node_modules')) {
+            return 'vendor';
+          }
+        },
+      },
+    },
+  },
 });
